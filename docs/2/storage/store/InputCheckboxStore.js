a5771f3853efbe218a21254a6eca4020f48bfa2c
ytyaru <ytyaru@outlook.jp>
2021-02-12 19:34:10 +0900

    リファクタリング。InputCheckboxStore

--- docs/2/storage/store/InputCheckboxStore.js
+++ docs/2/storage/store/InputCheckboxStore.js
@@ -1,6 +1,5 @@
 class InputCheckboxStore {
     static getElements() { return document.querySelectorAll(`input[type="checkbox"]`); }
-    static #getCheckedElements() { return document.querySelectorAll(`input[type="checkbox"]:checked`); }
     static getKeys(elements) {
         return new Set(Array.from(elements).map(element=>element.getAttribute('name')));
     }
@@ -11,41 +10,26 @@ class InputCheckboxStore {
         const values = Array.from(InputCheckboxStore.getElements())
                             .filter(element=>element.checked)
                             .map(element=>element.getAttribute('value') || document.querySelector(`label[for="${element.getAttribute('id')}"]`).textContent);
-        console.log('getValue()', values);
         return JSON.stringify(values);
-        /*
-        Array.from(InputCheckboxStore.getElements()).map(element=>element.checked)
-        for (let e of InputCheckboxStore.getElements()) {
-            if (e.checked) {
-        }
-        console.log();
-        const checks = InputCheckboxStore.getElements().querySelectorAll(`:checked`);
-        const values = Array.from(checks).map(el=>el.getAttribute('value') || document.querySelector(`label[for="${el.getAttribute('id')}"]`).textContent);
-        console.log(values);
-        return values;
-        */
     }
     static setValue(element, value) {
-        console.log('setValue()', value);
         const values = JSON.parse(value);
-        console.log(values);
         if (!values) { return; }
-        console.log(values);
+        InputCheckboxStore.#clearChecked(element);
         for (let value of values) {
             const checks = Array.from(InputCheckboxStore.getElements())
                                 .filter(element=>value === element.getAttribute('value'));
             for (let element of checks) {
-                element.setAttribute('checked', 'checked');
+                element.checked = true;
             }
         }
-        /*
-        const v = element.getAttribute('value');
-        const l = document.querySelector(`label[for="${element.getAttribute('id')}"]`).textContent;
-        if (values.indexOf(v) || values.indexOf(l)) {
-            element.setAttribute('checked', 'checked');
-        }
-        */
     }
-//    static isSaveTarget(element) { return element.checked; }
+    static #clearChecked(target) {
+        for (let element of InputCheckboxStore.getElements()) {
+            if (target.getAttribute('name') === target.getAttribute('name')) {
+                if (element.checked) { element.checked = false; }
+            }
+        }
+    }
     static isSaveTarget(element) { return true; }
 }
